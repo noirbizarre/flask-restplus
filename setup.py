bf32c0caad488ecddea4ecc37000faeeb1dccc7b
Axel Haustant <noirbizarre@gmail.com>
2014-08-17 21:17:49 +0200

    Removed useless setup.py code

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 from __future__ import unicode_literals
 import re
 
-from os.path import join
-
 from setuptools import setup, find_packages
 
 RE_REQUIREMENT = re.compile(r'^\s*-r\s*(?P<filename>.*)$')
@@ -32,25 +30,6 @@ def rst(filename):
     for regex, replacement in PYPI_RST_FILTERS:
         content = re.sub(regex, replacement, content)
     return content
-
-
-def pip(filename):
-    '''Parse pip requirement file and transform it to setuptools requirements'''
-    requirements = []
-    for line in open(join('requirements', filename)):
-        line = line.strip()
-        if not line or '://' in line:
-            continue
-        match = RE_REQUIREMENT.match(line)
-        if match:
-            requirements.extend(pip(match.group('filename')))
-        else:
-            requirements.append(line)
-    return requirements
-
-
-def dependency_links(filename):
-    return [line.strip() for line in open(join('requirements', filename)) if '://' in line]
 
 
 long_description = '\n'.join((
