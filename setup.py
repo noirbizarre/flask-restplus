79f79aec19e651c50f27d533751934da60339247
Axel Haustant <noirbizarre@gmail.com>
2017-01-15 11:56:03 +0100

    Compatible with Python 3.6

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
