48323fc31e04cf5976313c10978ad6f7a221cfd5
Calvin Wu <calvin620707@msn.com>
2017-09-28 07:54:25 +0800

    try to fix the issue of travis CI fail on 3.3
    https://github.com/travis-ci/docs-travis-ci-com/issues/982

--- tasks.py
+++ tasks.py
@@ -41,7 +41,7 @@ def test(ctx, profile=False):
         '--benchmark-skip',
         '--profile' if profile else None,
     )
-    lrun('pytest {0}'.format(kwargs), pty=True)
+    lrun('py.test {0}'.format(kwargs), pty=True)
 
 
 @task
