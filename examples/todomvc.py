fdfa3c06d079337e5df7e0d64aecffbfac75f870
Kamforka <kamforka@hotmail.com>
2018-06-20 16:13:40 +0200

    Todo model's id readonly property typo fix
    The `readonly` property had a typo, corrected that, so now the swagger UI displays the example API payload correctly (without the id field), as originally intended.

--- examples/todomvc.py
+++ examples/todomvc.py
@@ -11,7 +11,7 @@ api = Api(app, version='1.0', title='TodoMVC API',
 ns = api.namespace('todos', description='TODO operations')
 
 todo = api.model('Todo', {
-    'id': fields.Integer(readOnly=True, description='The task unique identifier'),
+    'id': fields.Integer(readonly=True, description='The task unique identifier'),
     'task': fields.String(required=True, description='The task details')
 })
 
