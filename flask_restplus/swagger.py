847664e1b144a3da82e243b8fdf11b7677382e71
illes <illes.solt@gmail.com>
2019-03-30 16:42:51 +0100

    fix missing definition of nested ordered model
    Example UI error message:
    ```
    Could not resolve pointer: /definitions/XXX does not exist in document
    ```

--- flask_restplus/swagger.py
+++ flask_restplus/swagger.py
@@ -12,7 +12,7 @@ from flask import current_app
 from werkzeug.routing import parse_rule
 
 from . import fields
-from .model import Model, ModelBase
+from .model import Model, ModelBase, OrderedModel
 from .reqparse import RequestParser
 from .utils import merge, not_none, not_none_sorted
 from ._http import HTTPStatus
@@ -46,7 +46,8 @@ RE_RAISES = re.compile(r'^:raises\s+(?P<name>[\w\d_]+)\s*:\s*(?P<description>.*)
 
 def ref(model):
     '''Return a reference to model in definitions'''
-    name = model.name if isinstance(model, ModelBase) else model
+    name = model.name if 
+    nce(model, ModelBase) else model
     return {'$ref': '#/definitions/{0}'.format(name)}
 
 
@@ -537,7 +538,7 @@ class Swagger(object):
         if isinstance(specs, ModelBase):
             for parent in specs.__parents__:
                 self.register_model(parent)
-        if isinstance(specs, Model):
+        if isinstance(specs, (Model, OrderedModel)):
             for field in itervalues(specs):
                 self.register_field(field)
         return ref(model)
