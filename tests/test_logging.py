9cb2ad8cbb924f5ec963a51b52158ca8d4eb3ba1
Ben Steadman <steadmanben1@gmail.com>
2019-09-08 07:50:38 +0100

    namespace logging handler test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -92,3 +92,42 @@ class LoggingTest(object):
         client.get("/ns2/")
         matching = [r for r in caplog.records if r.message == "hello from ns2"]
         assert len(matching) == 0
+
+    def test_namespace_additional_handler(self, app, client, caplog, tmp_path):
+        caplog.set_level(logging.INFO, logger=app.logger.name)
+        log_file = tmp_path / "v1.log"
+
+        api = restplus.Api(app)
+        ns1 = api.namespace('ns1', path='/ns1')
+        # set up a file handler for ns1 only
+        # FileHandler only supports Path object on Python >= 3.6 -> cast to str
+        fh = logging.FileHandler(str(log_file))
+        ns1.logger.addHandler(fh)
+
+        ns2 = api.namespace('ns2', path='/ns2')
+
+        @ns1.route('/')
+        class Ns1(restplus.Resource):
+            def get(self):
+                ns1.logger.info("hello from ns1")
+                pass
+
+        @ns2.route('/')
+        class Ns2(restplus.Resource):
+            def get(self):
+                ns2.logger.info("hello from ns2")
+                pass
+
+        # ns1 logs go to stdout and log_file
+        client.get("/ns1/")
+        matching = [r for r in caplog.records if r.message == "hello from ns1"]
+        assert len(matching) == 1
+        with log_file.open() as f:
+            assert "hello from ns1" in f.read()
+
+        # ns2 logs go to stdout only
+        client.get("/ns2/")
+        matching = [r for r in caplog.records if r.message == "hello from ns2"]
+        assert len(matching) == 1
+        with log_file.open() as f:
+            assert "hello from ns1" in f.read()
