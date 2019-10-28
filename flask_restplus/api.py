de38220655d159a1dc43fd9a305a6903f62c6e52
Julien Castets <castets.j@gmail.com>
2019-10-28 22:22:38 +0100

    Don't return exception when errorhandler is set (fix #693) (#741)
    If errorhandler is configured, error should not be returned to client
    even if PROPAGATE_EXCEPTIONS is true.

--- flask_restplus/api.py
+++ flask_restplus/api.py
@@ -609,7 +609,12 @@ class Api(object):
         '''
         got_request_exception.send(current_app._get_current_object(), exception=e)
 
-        if not isinstance(e, HTTPException) and current_app.propagate_exceptions:
+        # When propagate_exceptions is set, do not return the exception to the
+        # client if a handler is configured for the exception.
+        if not isinstance(e, HTTPException) and \
+                current_app.propagate_exceptions and \
+                not isinstance(e, tuple(self.error_handlers.keys())):
+
             exc_type, exc_value, tb = sys.exc_info()
             if exc_value is e:
                 raise
